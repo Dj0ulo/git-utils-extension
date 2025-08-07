19e29095ef7886414d26de1e7878eb7c956e08e8
Julien (jula) <jula@odoo.com>
2025-08-07 11:11:43 +0200

    [ADD] Open file in write mode

--- extension.js
+++ extension.js
@@ -6,6 +6,23 @@ function activate(context) {
   });
 
   context.subscriptions.push(disposable);
+
+  let openFileDisposable = vscode.commands.registerCommand('git-utils.openFileInWriteMode', async () => {
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+      const uri = editor.document.uri;
+      const fileUri = vscode.Uri.file(uri.path);
+      const selection = editor.selection;
+
+      const doc = await vscode.workspace.openTextDocument(fileUri);
+      const newEditor = await vscode.window.showTextDocument(doc, { preview: false });
+
+      newEditor.selection = selection;
+      newEditor.revealRange(selection, vscode.TextEditorRevealType.InCenter);
+    }
+  });
+
+  context.subscriptions.push(openFileDisposable);
 }
 
 function deactivate() {}
